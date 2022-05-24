c154b4e250dd0b73e115c0adcce7dce5b3f3c2d6
Michael Hirsch <michael@hirsch.me>
2022-05-24 03:13:35 -0500

    chore: remove dist from git

--- dist/dist/src/typechain-types/factories/contracts/ERC1155/ERC1155U.sol/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERC1155U__factory = exports.ERC1155TokenReceiver__factory = void 0;
-var ERC1155TokenReceiver__factory_1 = require("./ERC1155TokenReceiver__factory");
-Object.defineProperty(exports, "ERC1155TokenReceiver__factory", { enumerable: true, get: function () { return ERC1155TokenReceiver__factory_1.ERC1155TokenReceiver__factory; } });
-var ERC1155U__factory_1 = require("./ERC1155U__factory");
-Object.defineProperty(exports, "ERC1155U__factory", { enumerable: true, get: function () { return ERC1155U__factory_1.ERC1155U__factory; } });
