70a1a797edfe140185b429b59e01229480dd44c6
Michael Hirsch <michael@hirsch.me>
2022-05-27 17:38:03 -0500

    fix: typo

--- src/services/s3.ts
+++ src/services/s3.ts
@@ -10,7 +10,7 @@ export const uploadJsonToS3 = async (json: any, folder: S3Folder) => {
   const s3 = new S3({
     region: process.env.AWS_S3_REGION,
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRETE_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
   });
   const buffer = Buffer.from(JSON.stringify(json));
   const params = {
